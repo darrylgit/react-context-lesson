8e60d7447a72e12fce7eb49e85d3b804311b0053
darrylgit <darrylsmithup@gmail.com>
2020-03-22 18:11:32 -0400

    Access collections context with useContext

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,31 +1,24 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useContext } from "react";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
-import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionsContext from "../../contexts/collections/collections.context";
 
 import "./collection.styles.scss";
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
   return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <div className="collection-page">
-            <h2 className="title">{title}</h2>
-            <div className="items">
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
